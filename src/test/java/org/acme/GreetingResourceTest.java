f31684d9e901d8fdac66df510ad7fa378c1e1a95
Grant McCarthy <grantmc99@gmail.com>
2023-06-20 17:26:01 -0400

    chnages

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -12,10 +13,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello-resteasy")
+          .when().get("/hello-summit")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(containsString("Hello Charlotte"));
     }
 
 }
