bb583404949bd851f74b47e77adff473786c6ae6
Grant McCarthy <19212354+gmccarth@users.noreply.github.com>
2023-06-20 09:55:44 -0400

    Create GreetingResourceOrig.java

---
+++ src/main/java/org/acme/GreetingResourceOrig.java
@@ -0,0 +1,16 @@
+package org.acme;
+//import org.eclipse.microprofile.config.inject.ConfigProperty;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello-summit")
+public class GreetingResource {
+    //@ConfigProperty(name="SUMMIT_YEAR") String summitYear;
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Charlotte RHUG - Summit 2024 Recap" ;
+    }
+}
